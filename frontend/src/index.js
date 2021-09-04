8e5fa7bf64bd1a7ebc541e0779b9fcc64a173c03
nandakishorJ <pynanda19@gmail.com>
2021-09-04 21:33:45 +0530

    initial frontend app

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+
+const a = true
+ReactDOM.render(
+  <React.StrictMode>
+    {a ? <App /> : <h1>Hello World from JS in JSX</h1>}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
